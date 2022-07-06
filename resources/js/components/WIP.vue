b9b3e8e386aa770a9ef57d2be65d0baada24e275
@BagliettoAron <aronbaglietto88@gmail.com>
2022-07-06 08:51:13 +0200

    Add: vue

---
+++ resources/js/components/WIP.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="">
+<h1>wip</h1>
+<WIP />
+</div>
+  
+</template>
+
+<script>
+import WIP from "../components/WIP.vue"; 
+export default {
+    name:"App",
+    components: {
+        WIP,
+    },
+
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+<h2>app vue</h2>
+  
+</template>
+
+<script>
+export default {
+    name:'App'
+
+}
+</script>
+
+<style>
+
+</style>
